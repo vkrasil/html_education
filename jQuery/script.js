f868af2244ffd916be5309ee361fd398fa4a2d2d
vkrasil <krasilnikovv@gmail.com>
2022-04-23 22:55:09 +0300

    Code->Reformat made for .js files

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,93 +1,89 @@
 $(".about_member").hide()
 
-let MouseState= {
-    mbr1State:undefined,
-    mbr2State:undefined,
-    mbr3State:undefined,
-    mbr4State:undefined
+let MouseState = {
+    mbr1State: undefined,
+    mbr2State: undefined,
+    mbr3State: undefined,
+    mbr4State: undefined
 }
 
 $("#btn_row").click(function () {
-    $(".block_members").css("grid-template-columns","1fr 1fr 1fr 1fr")
+    $(".block_members").css("grid-template-columns", "1fr 1fr 1fr 1fr")
 });
 
 $("#btn_grid").click(function () {
-    $(".block_members").css("grid-template-columns","1fr 1fr")
+    $(".block_members").css("grid-template-columns", "1fr 1fr")
 });
 
-function mouseEnter (e)
-{
-    const memberNum=getNumberFromId(e.target.id)
+function mouseEnter(e) {
+    const memberNum = getNumberFromId(e.target.id)
     startEffect(memberNum)
-    setTimeout(intervalCheck,500,memberNum)
+    setTimeout(intervalCheck, 500, memberNum)
     MouseState[`mbr${memberNum}State`] = true
 }
 
-function mouseLeave(e)
-{
-    const memberNum=getNumberFromId(e.target.id)
+function mouseLeave(e) {
+    const memberNum = getNumberFromId(e.target.id)
     stopEffect(memberNum)
     MouseState[`mbr${memberNum}State`] = false
 }
 
-function intervalCheck(number){
-    if ((($(`#div_about${number}:hover`).length)===0) && (MouseState[`mbr${number}State`]))
-    {
-        const elemNum=number.toString()
+function intervalCheck(number) {
+    if ((($(`#div_about${number}:hover`).length) === 0) && (MouseState[`mbr${number}State`])) {
+        const elemNum = number.toString()
         $(`#div_about${elemNum}`).triggerHandler("mouseleave")
     }
 }
 
-function startEffect(memberNum)
-{
-    const targetImg= $(`#img_member${memberNum}`)
-    targetImg.fadeTo(1000,0.25)
+function startEffect(memberNum) {
+    const targetImg = $(`#img_member${memberNum}`)
+    targetImg.fadeTo(1000, 0.25)
     $(`.div_member${memberNum} p`).hide()
     const divAbout = $(`#div_about${memberNum}`)
-    const divMember=$(`.div_member${memberNum}`)
+    const divMember = $(`.div_member${memberNum}`)
     const boxDiv = divMember[0].getBoundingClientRect()
-    const boxImage=targetImg[0].getBoundingClientRect()
+    const boxImage = targetImg[0].getBoundingClientRect()
     divMember.height(boxDiv.height)
     divMember.width(boxDiv.width)
-    const screenScrollX=window.scrollX
-    const screenScrollY=window.scrollY
-    targetImg.css({'position':'absolute'})
-    targetImg.offset({ top: boxImage.top+screenScrollY, left: boxImage.left+screenScrollX } )
+    const screenScrollX = window.scrollX
+    const screenScrollY = window.scrollY
+    targetImg.css({'position': 'absolute'})
+    targetImg.offset({top: boxImage.top + screenScrollY, left: boxImage.left + screenScrollX})
     targetImg.width(boxImage.width)
     targetImg.height(boxImage.height)
-    const offset=Math.round((boxDiv.height-boxImage.height)/2)
-    targetImg.animate({ "top": `+=${offset}px` },  {queue: false, duration: 500} )
+    const offset = Math.round((boxDiv.height - boxImage.height) / 2)
+    targetImg.animate({"top": `+=${offset}px`}, {queue: false, duration: 500})
     divAbout.css({
-        'top': boxDiv.top+screenScrollY,
-        'left': boxImage.left+screenScrollX,
+        'top': boxDiv.top + screenScrollY,
+        'left': boxImage.left + screenScrollX,
         'width': boxImage.width,
         'height': boxDiv.height
     })
     divAbout.delay(200).fadeIn(1000)
 }
 
-function stopEffect(memberNum)
-{
-    const imgMember=$(`#img_member${memberNum}`)
+function stopEffect(memberNum) {
+    const imgMember = $(`#img_member${memberNum}`)
     const divAbout = $(`#div_about${memberNum}`)
-    const divMember=$(`.div_member${memberNum}`)
+    const divMember = $(`.div_member${memberNum}`)
     const boxDiv = divMember[0].getBoundingClientRect()
-    const boxImage=imgMember[0].getBoundingClientRect()
-    const offset=Math.round((boxDiv.height-boxImage.height)/2)
+    const boxImage = imgMember[0].getBoundingClientRect()
+    const offset = Math.round((boxDiv.height - boxImage.height) / 2)
 
-    imgMember.animate({ "top": `-=${offset}px` },  {queue: false, duration: 500, complete: function (){
+    imgMember.animate({"top": `-=${offset}px`}, {
+        queue: false, duration: 500, complete: function () {
             imgMember.removeAttr('style')
             $(`.div_member${memberNum} p`).fadeIn(500)
-        }} )
-    imgMember.fadeTo(1000,1)
-    divAbout.fadeOut(1000,function (){
+        }
+    })
+    imgMember.fadeTo(1000, 1)
+    divAbout.fadeOut(1000, function () {
         divMember.removeAttr('style')
     })
 }
 
-function getNumberFromId(id)
-{
-    return id[id.length-1]
+function getNumberFromId(id) {
+    return id[id.length - 1]
 }
 
 $(".block_members img").on("mouseenter", mouseEnter)
--- js_animation/animation.js
+++ js_animation/animation.js
@@ -1,124 +1,112 @@
-let MouseState= {
-    mbr1State:undefined,
-    mbr2State:undefined,
-    mbr3State:undefined,
-    mbr4State:undefined
+let MouseState = {
+    mbr1State: undefined,
+    mbr2State: undefined,
+    mbr3State: undefined,
+    mbr4State: undefined
 }
 
-const imgMembers=document.querySelector('.block_members').getElementsByTagName('img')
-const divAbouts=document.getElementsByClassName('about_member')
+const imgMembers = document.querySelector('.block_members').getElementsByTagName('img')
+const divAbouts = document.getElementsByClassName('about_member')
 
 let isTouchScreen = "ontouchstart" in window || navigator.maxTouchPoints //detecting TouchScreen device
-if (isTouchScreen)
-{
+if (isTouchScreen) {
     //yes, it is TouchScreen
-    for (let eAbout of divAbouts)  eAbout.addEventListener("touchstart",onTouchStartDiv,false)
-    for (let eachImg of imgMembers)  eachImg.ontouchstart = onTouchStartImg
+    for (let eAbout of divAbouts) eAbout.addEventListener("touchstart", onTouchStartDiv, false)
+    for (let eachImg of imgMembers) eachImg.ontouchstart = onTouchStartImg
 
-} else
-{
+} else {
     //no, device without TouchScreen
-    for (let eachImg of imgMembers) eachImg.onmouseenter=onMouseMoveMember
-    for (let eachAbout of divAbouts) eachAbout.onmouseleave=onMouseLeaveMember
+    for (let eachImg of imgMembers) eachImg.onmouseenter = onMouseMoveMember
+    for (let eachAbout of divAbouts) eachAbout.onmouseleave = onMouseLeaveMember
 }
 
-function startEffect(numberOfMember)
-{
-    const targetImg=document.querySelector(`#img_member${numberOfMember}`)
-    const divMember=document.getElementsByClassName(`div_member${numberOfMember}`)[0]
+function startEffect(numberOfMember) {
+    const targetImg = document.querySelector(`#img_member${numberOfMember}`)
+    const divMember = document.getElementsByClassName(`div_member${numberOfMember}`)[0]
     const divAbout = document.getElementById(`div_about${numberOfMember}`)
     const boxDiv = divMember.getBoundingClientRect()
-    const boxImage=targetImg.getBoundingClientRect()
-    divMember.style.height=boxDiv.height.toString()+'px'
-    divMember.style.width=boxDiv.width.toString()+'px'
-    const screenScrollX=window.scrollX
-    const screenScrollY=window.scrollY
-    targetImg.style.position='absolute'
-    targetImg.style.left=(boxImage.left+screenScrollX).toString()+'px'
-    targetImg.style.top=(boxImage.top+screenScrollY).toString()+'px'
-    targetImg.style.width=(boxImage.width).toString()+'px'
-    targetImg.style.height=(boxImage.height).toString()+'px'
-    const offset=Math.round((boxDiv.height-boxImage.height)/2)
-    targetImg.style.transform=`translate(0,${offset}px)`
-    divAbout.style.left = (boxImage.left+screenScrollX).toString() + 'px'
-    divAbout.style.top = (boxDiv.top+screenScrollY).toString() + 'px'
+    const boxImage = targetImg.getBoundingClientRect()
+    divMember.style.height = boxDiv.height.toString() + 'px'
+    divMember.style.width = boxDiv.width.toString() + 'px'
+    const screenScrollX = window.scrollX
+    const screenScrollY = window.scrollY
+    targetImg.style.position = 'absolute'
+    targetImg.style.left = (boxImage.left + screenScrollX).toString() + 'px'
+    targetImg.style.top = (boxImage.top + screenScrollY).toString() + 'px'
+    targetImg.style.width = (boxImage.width).toString() + 'px'
+    targetImg.style.height = (boxImage.height).toString() + 'px'
+    const offset = Math.round((boxDiv.height - boxImage.height) / 2)
+    targetImg.style.transform = `translate(0,${offset}px)`
+    divAbout.style.left = (boxImage.left + screenScrollX).toString() + 'px'
+    divAbout.style.top = (boxDiv.top + screenScrollY).toString() + 'px'
     divAbout.style.width = boxImage.width.toString() + 'px'
     divAbout.style.height = boxDiv.height.toString() + 'px'
-    const divPName=document.getElementById(`dm${numberOfMember}p_name`)
-    const divPPosition=document.getElementById(`dm${numberOfMember}p_pos`)
-    divPName.style.opacity='0%'
-    divPPosition.style.opacity='0%'
-    divPName.style.visibility='hidden'
-    divPPosition.style.visibility='hidden'
-    divAbout.style.visibility='visible'
-    divAbout.style.opacity='100%'
+    const divPName = document.getElementById(`dm${numberOfMember}p_name`)
+    const divPPosition = document.getElementById(`dm${numberOfMember}p_pos`)
+    divPName.style.opacity = '0%'
+    divPPosition.style.opacity = '0%'
+    divPName.style.visibility = 'hidden'
+    divPPosition.style.visibility = 'hidden'
+    divAbout.style.visibility = 'visible'
+    divAbout.style.opacity = '100%'
     divMember.style.opacity = '25%'
 }
 
-function stopEffect(numberOfMember)
-{
-    const divMember=document.getElementsByClassName(`div_member${numberOfMember}`)[0]
+function stopEffect(numberOfMember) {
+    const divMember = document.getElementsByClassName(`div_member${numberOfMember}`)[0]
     const divAbout = document.getElementById(`div_about${numberOfMember}`)
-    const imgMember=document.getElementById(`img_member${numberOfMember}`)
-    imgMember.style.cssText=''
-    divAbout.style.opacity='0%'
-    divAbout.style.visibility='hidden'
-    const divPName=document.getElementById(`dm${numberOfMember}p_name`)
-    const divPPosition=document.getElementById(`dm${numberOfMember}p_pos`)
-    divPName.style.visibility='visible'
-    divPPosition.style.visibility='visible'
-    divPName.style.opacity='100%'
-    divPPosition.style.opacity='100%'
-    divMember.style.cssText=''
-    divMember.style.opacity='100%'
+    const imgMember = document.getElementById(`img_member${numberOfMember}`)
+    imgMember.style.cssText = ''
+    divAbout.style.opacity = '0%'
+    divAbout.style.visibility = 'hidden'
+    const divPName = document.getElementById(`dm${numberOfMember}p_name`)
+    const divPPosition = document.getElementById(`dm${numberOfMember}p_pos`)
+    divPName.style.visibility = 'visible'
+    divPPosition.style.visibility = 'visible'
+    divPName.style.opacity = '100%'
+    divPPosition.style.opacity = '100%'
+    divMember.style.cssText = ''
+    divMember.style.opacity = '100%'
 }
 
-function onTouchStartImg (te)
-{
-    const numberOfMember=te.target.id[te.target.id.length-1]
+function onTouchStartImg(te) {
+    const numberOfMember = te.target.id[te.target.id.length - 1]
     MouseState[`mbr${numberOfMember}State`] = true
-    if (isOtherMemberActive(numberOfMember))  inactiveMembersExcept(numberOfMember)
+    if (isOtherMemberActive(numberOfMember)) inactiveMembersExcept(numberOfMember)
     startEffect(numberOfMember)
 }
 
-function onTouchStartDiv (te)
-{
-    const str1=te.target.id[te.target.id.length-1]
+function onTouchStartDiv(te) {
+    const str1 = te.target.id[te.target.id.length - 1]
     let str2
     let numberOfMember
-    if (te.isTrusted)
-    {
-        str2=te.targetTouches[0].target.parentElement.id[te.targetTouches[0].target.parentElement.id.length-1]
-        numberOfMember=(str1) || (str2)
-    }else
-    {
-        numberOfMember=str1
+    if (te.isTrusted) {
+        str2 = te.targetTouches[0].target.parentElement.id[te.targetTouches[0].target.parentElement.id.length - 1]
+        numberOfMember = (str1) || (str2)
+    } else {
+        numberOfMember = str1
     }
     MouseState[`mbr${numberOfMember}State`] = false
     stopEffect(numberOfMember)
 }
 
-function isOtherMemberActive(number){
-    let result=false
-    for (let currentKey=1;currentKey<=Object.keys(MouseState).length; currentKey++)
-    {
-        if ((MouseState[`mbr${currentKey}State`] === true) && (number!==currentKey)) result=true
+function isOtherMemberActive(number) {
+    let result = false
+    for (let currentKey = 1; currentKey <= Object.keys(MouseState).length; currentKey++) {
+        if ((MouseState[`mbr${currentKey}State`] === true) && (number !== currentKey)) result = true
     }
     return result
 }
 
-function inactiveMembersExcept(exceptNum){
+function inactiveMembersExcept(exceptNum) {
     for (let eachAbout of divAbouts) {
-        let currentMemberNum=eachAbout.id[eachAbout.id.length-1]
-        if ((currentMemberNum!==exceptNum)&&(MouseState[`mbr${currentMemberNum}State`] === true))
-        {
-            if (isTouchScreen)
-            {
-                let tEvent=new TouchEvent("touchstart")
+        let currentMemberNum = eachAbout.id[eachAbout.id.length - 1]
+        if ((currentMemberNum !== exceptNum) && (MouseState[`mbr${currentMemberNum}State`] === true)) {
+            if (isTouchScreen) {
+                let tEvent = new TouchEvent("touchstart")
                 eachAbout.dispatchEvent(tEvent)
-            } else
-            {
-                let mEvent=new MouseEvent("mouseleave")
+            } else {
+                let mEvent = new MouseEvent("mouseleave")
                 eachAbout.dispatchEvent(mEvent)
             }
         }
@@ -126,41 +114,40 @@ function inactiveMembersExcept(exceptNum){
     }
 }
 
-function intervalCheck(number){
-    if (!(document.querySelector(`#div_about${number}:hover`)) && (MouseState[`mbr${number}State`]))
-    {
-        let mEvent=new MouseEvent("mouseleave")
-        divAbouts[(number-1).toString()].dispatchEvent(mEvent)
+function intervalCheck(number) {
+    if (!(document.querySelector(`#div_about${number}:hover`)) && (MouseState[`mbr${number}State`])) {
+        let mEvent = new MouseEvent("mouseleave")
+        divAbouts[(number - 1).toString()].dispatchEvent(mEvent)
     }
 
 }
 
 function onMouseMoveMember(e) {
-    const numberOfMember=e.target.id[e.target.id.length-1]
+    const numberOfMember = e.target.id[e.target.id.length - 1]
     if ((MouseState[`mbr${numberOfMember}State`] === undefined) || (MouseState[`mbr${numberOfMember}State`] === false)) {
-        setTimeout(intervalCheck,500,numberOfMember)
-        if (isOtherMemberActive(numberOfMember))  inactiveMembersExcept(numberOfMember)
+        setTimeout(intervalCheck, 500, numberOfMember)
+        if (isOtherMemberActive(numberOfMember)) inactiveMembersExcept(numberOfMember)
         startEffect(numberOfMember)
         MouseState[`mbr${numberOfMember}State`] = true
     }
 }
 
-function onMouseLeaveMember (e) {
-    const numberOfMember=e.target.id[e.target.id.length-1]
+function onMouseLeaveMember(e) {
+    const numberOfMember = e.target.id[e.target.id.length - 1]
     MouseState[`mbr${numberOfMember}State`] = false
     stopEffect(numberOfMember)
 }
 
-const btnRow=document.getElementById('btn_row')
-const btnGrid=document.getElementById('bnt_grid')
+const btnRow = document.getElementById('btn_row')
+const btnGrid = document.getElementById('bnt_grid')
 
-btnRow.onclick=()=>{
-    const blockMembers=document.querySelector('.block_members')
-    blockMembers.style.gridTemplateColumns='1fr 1fr 1fr 1fr'
+btnRow.onclick = () => {
+    const blockMembers = document.querySelector('.block_members')
+    blockMembers.style.gridTemplateColumns = '1fr 1fr 1fr 1fr'
 
 }
 
-btnGrid.onclick=()=>{
-    const blockMembers=document.querySelector('.block_members')
-    blockMembers.style.gridTemplateColumns='1fr 1fr'
+btnGrid.onclick = () => {
+    const blockMembers = document.querySelector('.block_members')
+    blockMembers.style.gridTemplateColumns = '1fr 1fr'
 }
